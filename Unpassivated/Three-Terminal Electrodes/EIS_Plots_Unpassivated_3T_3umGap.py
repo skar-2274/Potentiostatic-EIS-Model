b836617c05e621485c7c714d59595fc22b80d805
Surajit Kar <40043056+skar-2274@users.noreply.github.com>
2024-09-11 00:34:44 +0100

    Update EIS_Plots_Unpassivated_3T_3umGap.py

--- Unpassivated/Three-Terminal Electrodes/EIS_Plots_Unpassivated_3T_3umGap.py
+++ Unpassivated/Three-Terminal Electrodes/EIS_Plots_Unpassivated_3T_3umGap.py
@@ -11,11 +11,11 @@ frequencies = np.logspace(6, -1, num=70)
 
 # Mathematical Formalism
 def impedance(f, R_s, R_ct, C_dl, A_w):
-    Z_w = A_w / np.sqrt(1j * 2 * np.pi * f)  # Warburg impedance
-    Z_series = R_ct + Z_w                    # Series combination of R_ct and Z_w
-    Z_C = 1j * 2 * np.pi * f * C_dl          # Capacitor Impedance
-    Z_parallel = 1 / ((Z_series ** -1) + Z_C)    # Parallel combination with capacitance
-    Z_total = R_s + Z_parallel               # Series combination of R_s and parallel part
+    Z_w = A_w / np.sqrt(1j * 2 * np.pi * f)   # Warburg impedance
+    Z_series = R_ct + Z_w                     # Series combination of R_ct and Z_w
+    Z_C = 1j * 2 * np.pi * f * C_dl           # Capacitor impedance
+    Z_parallel = 1 / ((Z_series ** -1) + Z_C) # Parallel combination with capacitance
+    Z_total = R_s + Z_parallel                # Series combination of R_s and parallel part
     return Z_total
 
 real_values = []
